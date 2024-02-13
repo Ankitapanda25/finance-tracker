9e002a78377522b519fe226cfb913e0b4aeab5a6
Ankita Panda <ankitapanda2502@gmail.com>
2024-02-14 01:38:28 +0530

    first commit

---
+++ server/DB/Database.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDB = async (req, res) => {
+    const db = "mongodb+srv://ankita25:ankita2016@cluster0.kctgqri.mongodb.net/test?retryWrites=true&w=majority";
+
+    const {connection} = await mongoose.connect(db, { useNewUrlParser: true });
+
+    console.log(`MongoDB Connected to ${connection.host}`);
+
+}
