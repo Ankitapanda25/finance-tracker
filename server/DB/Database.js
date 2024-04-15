e276a502fd45f6edfba253c61895381be5839f09
Ankita Panda <ankitapanda2502@gmail.com>
2024-04-15 23:41:25 +0530

    changes

--- server/DB/Database.js
+++ server/DB/Database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async (req, res) => {
     const db = "mongodb+srv://ankita25:ankita2016@cluster0.kctgqri.mongodb.net/test?retryWrites=true&w=majority";
 
-    const {connection} = await mongoose.connect(db, { useNewUrlParser: true });
+    const {connection} = await mongoose.connect(db);
 
     console.log(`MongoDB Connected to ${connection.host}`);
 
